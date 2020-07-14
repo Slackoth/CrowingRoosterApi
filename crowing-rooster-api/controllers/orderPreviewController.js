b4f3c7e8877282d527448973a319dbf10d53128e
Slackoth <00082318@uca.edu.sv>
2020-07-14 15:16:27 -0600

    a saber que era

--- crowing-rooster-api/controllers/orderPreviewController.js
+++ crowing-rooster-api/controllers/orderPreviewController.js
@@ -3,16 +3,15 @@ var db = require('../db/connection')
 const getSuccessfulOrderPreview = async (req,res) => {
     const code = req.query.codigo
 
-    await db.connection.any(`select o.codigo_orden, 
-    oe.fecha_entrega as fecha, sum(p.cantidad_bateria) as cantidad,
-    o.estado, c2.codigo as comprador, b2.product_img as img
-    from orden o inner join orden_exitosa oe 
-    on o.codigo_orden = oe.id_oexitosa inner join pedido p 
+    await db.connection.any(`select o.codigo_orden, op.fecha_pendiente, sum(p.cantidad_bateria) as cantidad,
+    o.estado,c2.codigo as comprador,b2.product_img as img
+    from orden o inner join orden_pendiente op 
+    on o.codigo_orden = op.id_opendiente inner join pedido p 
     on o.codigo_orden = p.codigo_orden inner join comprador c2 
     on p.comprador_codigo = c2.codigo inner join bateria b2 
     on p.id_bateria = b2.id_bateria 
     where c2.codigo = '${code}' and o.estado = 'Exitosa'
-    group by o.codigo_orden, oe.fecha_entrega,c2.codigo,b2.product_img;`
+    group by o.codigo_orden, op.fecha_pendiente,c2.codigo,b2.product_img ;`
     )
     .then(data => {
         res.status(200).json(data)
@@ -28,16 +27,15 @@ const getOngoingOrderPreview = async (req,res) => {
     console.log(code);
     
 
-    await db.connection.any(`select o.codigo_orden, 
-    oe.fecha_entrega as fecha, sum(p.cantidad_bateria) as cantidad,
-    o.estado, c2.codigo as comprador, b2.product_img as img
-    from orden o inner join orden_exitosa oe 
-    on o.codigo_orden = oe.id_oexitosa inner join pedido p 
+    await db.connection.any(`select o.codigo_orden, op.fecha_pendiente, sum(p.cantidad_bateria) as cantidad,
+    o.estado,c2.codigo as comprador,b2.product_img as img
+    from orden o inner join orden_pendiente op 
+    on o.codigo_orden = op.id_opendiente inner join pedido p 
     on o.codigo_orden = p.codigo_orden inner join comprador c2 
     on p.comprador_codigo = c2.codigo inner join bateria b2 
     on p.id_bateria = b2.id_bateria 
     where c2.codigo = '${code}' and o.estado = 'Pendiente'
-    group by o.codigo_orden, oe.fecha_entrega,c2.codigo,b2.product_img;`
+    group by o.codigo_orden, op.fecha_pendiente,c2.codigo,b2.product_img ;`
     )
     .then(data => {
         res.status(200).json(data)
