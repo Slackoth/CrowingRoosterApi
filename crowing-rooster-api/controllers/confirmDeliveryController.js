57d64f21ed02afae3873ad11ad96b4dc80cf6d73
Slackoth <00082318@uca.edu.sv>
2020-07-14 16:17:09 -0600

    confirmar entrega

--- crowing-rooster-api/controllers/confirmDeliveryController.js
+++ crowing-rooster-api/controllers/confirmDeliveryController.js
@@ -3,10 +3,13 @@ var db = require('../db/connection')
 const confirmDelivery = async (req,res) => {
     const code = req.query.codigo
     const id = req.query.entregaId
+    const body = req.body
     console.log(code);
     console.log(id);
+    console.log(body);
+    
 
-    await db.connection.any(`select * from confirmar_entrega('${code}',${id})`)
+    await db.connection.any(`select * from confirmar_entrega('${code}',${id},${body.price},'${body.payment}')`)
     .then(data => {
         return res.status(200).send(data)
     })
